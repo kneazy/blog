cc5a70696fb19cab9da9a1a957a775d6d7166d80
kneazy <kneazy2011@gmail.com>
2018-04-27 15:53:04 +0300

    optimization

--- src/CreatePost.js
+++ src/CreatePost.js
@@ -3,98 +3,98 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 class CreatePost extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-          inputTitle:'',
-          inputBody:'',
-          inputAuthorsId: 1,
-          userName:[],
-            
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChangeBody = this.handleChangeBody.bind(this);
-        this.handleChangeAuthorsId = this.handleChangeAuthorsId.bind(this);
-    }
-
-    componentDidMount() {
-        axios.get('http://localhost:4000/authors/' )
-        .then((data) => {
-            this.setState({
-                userName: data.data
-            })
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    }
-
-    handleChange(e) {
-        e.preventDefault();
-        this.setState({
-            inputTitle: e.target.value
-        })
-    }
-
-    handleChangeBody(e) {
-        e.preventDefault();
-        this.setState({
-            inputBody: e.target.value
-        })
-    }
-
-    handleChangeAuthorsId(e) {
-        e.preventDefault();
-        this.setState({
-            inputAuthorsId: parseInt(e.target.value, 10)
-        })
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
+  constructor(props){
+    super(props);
+    this.state = {
+      inputTitle:'',
+      inputBody:'',
+      inputAuthorsId: 1,
+      userName:[],
         
-        axios.post('http://localhost:4000/posts/', {
-                title: this.state.inputTitle,
-                body: this.state.inputBody,
-                authorId: this.state.inputAuthorsId
-        })
-        .then((data) => {
-            console.log(data.data);            
-        })
-        .catch((error) => {
-            console.log(error);
-        });
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeBody = this.handleChangeBody.bind(this);
+    this.handleChangeAuthorsId = this.handleChangeAuthorsId.bind(this);
+  }
 
-    render() {
-        const user = this.state.userName.map((item, i)=>{
-                        return(  
-                            <option key={i} value={item.id}>{item.first_name} {item.last_name}</option>                       
-                        );
-                    })
-        return (
-            <div className='wrapper'>
-                <h1>CreatePost</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <label><b>Title:</b><br/>
-                        <input type='text' size='40' onChange = {this.handleChange} />
-                    </label>
-                    <br/>
-                    <label><b>Post:</b><br/>
-                        <textarea name='comment' cols='41' rows='3' onChange = {this.handleChangeBody}/>
-                    </label>
-                    <br/>
-                    <b>Select Author:</b>
-                    <select value={this.state.inputAuthorsId} onChange = {this.handleChangeAuthorsId}>
-                        {user}
-                    </select>
-                    <input type='submit' />
-                </form>
-                <h4><Link to='/'>Back Home</Link></h4>
-            </div>
+  componentDidMount() {
+    axios.get('http://localhost:4000/authors/' )
+    .then((data) => {
+        this.setState({
+            userName: data.data
+        })
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({
+        inputTitle: e.target.value
+    })
+  }
+
+  handleChangeBody(e) {
+    e.preventDefault();
+    this.setState({
+        inputBody: e.target.value
+    })
+  }
+
+  handleChangeAuthorsId(e) {
+    e.preventDefault();
+    this.setState({
+        inputAuthorsId: parseInt(e.target.value, 10)
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    
+    axios.post('http://localhost:4000/posts/', {
+            title: this.state.inputTitle,
+            body: this.state.inputBody,
+            authorId: this.state.inputAuthorsId
+    })
+    .then((data) => {
+      console.log(data.data);            
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  render() {
+      const user = this.state.userName.map((item, i)=>{
+        return(  
+            <option key={i} value={item.id}>{item.first_name} {item.last_name}</option>                       
         );
-    }
+      })
+    return (
+      <div className='wrapper'>
+        <h1>CreatePost</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label><b>Title:</b><br/>
+            <input type='text' size='40' onChange = {this.handleChange} />
+          </label>
+          <br/>
+          <label><b>Post:</b><br/>
+            <textarea name='comment' cols='41' rows='3' onChange = {this.handleChangeBody}/>
+          </label>
+          <br/>
+          <b>Select Author:</b>
+          <select value={this.state.inputAuthorsId} onChange = {this.handleChangeAuthorsId}>
+            {user}
+          </select>
+          <input type='submit' />
+        </form>
+        <h4><Link to='/'>Back Home</Link></h4>
+      </div>
+    );
+  }
 }
 
 export default CreatePost;
--- src/PostPage.js
+++ src/PostPage.js
@@ -4,147 +4,144 @@ import axios from 'axios';
 import { withRouter, Link } from 'react-router-dom';
 class PostPage extends Component {
         constructor(props){
-            super(props);
-            this.state = {
-              postData:[],
-              userName:[],
-              selectLink:'',
-              commentsPost:[],
-              intputComments:'',
-              inputName:'',
-              inputPostId:0,
-
-                
-            }
-            this.handleChangeName = this.handleChangeName.bind(this);
-            this.handleSubmit = this.handleSubmit.bind(this);
-            this.handleChangeComment = this.handleChangeComment.bind(this);
-            
+          super(props);
+          this.state = {
+            postData:[],
+            userName:[],
+            selectLink:'',
+            commentsPost:[],
+            intputComments:'',
+            inputName:'',
+            inputPostId:0,    
+          }
+          this.handleChangeName = this.handleChangeName.bind(this);
+          this.handleSubmit = this.handleSubmit.bind(this);
+          this.handleChangeComment = this.handleChangeComment.bind(this);    
         }
 
-    componentDidMount() {
-        const {params} = this.props.match;    
-        axios.get('http://localhost:4000/posts/'+params.id)
-        .then((data) => {
-            this.setState({
-                postData: data.data
-            })
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-        
-        
-        axios.get('http://localhost:4000/authors/' )
-        .then((data) => {
-            this.setState({
-                userName: data.data
-            })
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-
-        axios.get('http://localhost:4000/comments/' )
-        .then((data) => {
-            this.setState({
-              commentsPost: data.data
-            })
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    }
-    handleChangeName(e) {
-      e.preventDefault();
-      this.setState({
-        inputName: e.target.value,
-        inputPostId: this.state.postData.id
-      })
-      
-    }
-
-    handleChangeComment(e) {
-        e.preventDefault();
+  componentDidMount() {
+    const {params} = this.props.match;
+    axios.get('http://localhost:4000/posts/'+params.id)
+    .then((data) => {
+        const authorId = data.data.authorId;
         this.setState({
-          inputComments: e.target.value
+          postData: data.data,
         })
-    }
 
-    handleSubmit(e) {
-      axios.post('http://localhost:4000/comments/', {
-              first_name: this.state.inputName,
-              comment: this.state.inputComments,
-              postId: this.state.inputPostId
-      })
+      axios.get('http://localhost:4000/authors/', {
+          params:{
+            id: authorId
+          }
+        })
       .then((data) => {
-          console.log(data.data);            
+        this.setState({
+          userName: data.data
+        })
       })
       .catch((error) => {
-          console.log(error);
+        console.log(error);
       });
-    }
 
-    authorsInfo(){
-        const id = this.state.postData.authorId;
-        return (
-            this.state.userName.map((item, i) => {
-              if(id === item.id){
-                return( 
-                  <div key = {i} className='authors'>
-                      <h4>Author:</h4><br/>
-                      <h3><i>{item.first_name} {item.last_name}</i></h3><br/>
-                      <i>{item.bio}</i>
-                  </div>
-                );
-              }
-              return null;
-            })       
+      axios.get('http://localhost:4000/comments/', {
+          params:{
+            postId: params.id
+          }
+      })
+      .then((data) => {
+        this.setState({
+          commentsPost: data.data
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+  }
+
+  handleChangeName(e) {
+    e.preventDefault();
+    this.setState({
+      inputName: e.target.value,
+      inputPostId: this.state.postData.id
+    })  
+  }
+
+  handleChangeComment(e) {
+    e.preventDefault();
+    this.setState({
+      inputComments: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    axios.post('http://localhost:4000/comments/', {
+      first_name: this.state.inputName,
+      comment: this.state.inputComments,
+      postId: this.state.inputPostId
+    })
+    .then((data) => {
+      console.log(data.data);            
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  authorsInfo(){
+    return (
+      this.state.userName.map((item, i) => {
+        return( 
+          <div key = {i} className='authors'>
+              <h4>Author:</h4><br/>
+              <h3><i>{item.first_name} {item.last_name}</i></h3><br/>
+              <i>{item.bio}</i>
+          </div>
         );
-    }
+      })       
+    );
+  }
 
-    shouComment(){
-      const postId = this.state.postData.id;
-      return (
-          this.state.commentsPost.map((item, i)=>{
-            if(postId === item.postId){
-              return (
-                <div key={i} className='commmets'>
-                    <h3><i>{item.first_name} :</i></h3>
-                    <p>{item.comment}</p>
-                </div>
-              );
-            }
-            return null;
-          })
-      );
-    }
-
-    render() {
-        const comments = this.shouComment();
-        const user = this.authorsInfo();
+  shouComment(){
+    return (
+      this.state.commentsPost.map((item, i) => {
         return (
-            <div className="wrapper">
-                <h1>PostPage</h1>
-                <div>{this.state.postData.body}</div>
-                <div>
-                    <h4><Link to='/'>Back Home</Link></h4>
-                    <div>{user}</div>
-                </div>
-                <div>{comments}</div>
-                <form onSubmit={this.handleSubmit }className="comment-form">
-                    <label><b>Name:</b><br/>
-                        <input type='text' size='53' onChange = {this.handleChangeName} />
-                    </label>
-                    <br/>
-                    <label><b>Comment:</b><br/>
-                        <textarea name='comment' cols='54' rows='3' onChange = {this.handleChangeComment}/>
-                    </label>
-                        <input type='submit'/>
-                </form>
+          <div key={i} className='commmets'>
+              <h3><i>{item.first_name} :</i></h3>
+              <p>{item.comment}</p>
+          </div>
+        );
+      })
+    );
+  }
+
+  render() {
+    const comments = this.shouComment();
+    const user = this.authorsInfo();
+    return (
+        <div className="wrapper">
+            <h1>PostPage</h1>
+            <div>{this.state.postData.body}</div>
+            <div>
+                <h4><Link to='/'>Back Home</Link></h4>
+                <div>{user}</div>
             </div>
-        );
-    }
+            <div>{comments}</div>
+            <form onSubmit={this.handleSubmit }className="comment-form">
+                <label><b>Name:</b><br/>
+                    <input type='text' size='53' onChange = {this.handleChangeName} />
+                </label>
+                <br/>
+                <label><b>Comment:</b><br/>
+                    <textarea name='comment' cols='54' rows='3' onChange = {this.handleChangeComment}/>
+                </label>
+                    <input type='submit'/>
+            </form>
+        </div>
+    );
+  }
 }
 
 export default withRouter(PostPage);
